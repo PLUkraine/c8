634cce33c929411d22cba6d0efe261cd9aaafaba
PLUkraine <plukraine001@gmail.com>
2021-05-31 09:51:59 -0700

    Change libcxx version for macos

--- conan_build.sh
+++ conan_build.sh
@@ -5,8 +5,15 @@ set -eux
 mkdir -p build
 
 pushd build
-conan install .. --build=missing -s compiler.libcxx=libstdc++11
+case "$(uname -s)" in
+    Linux)
+        conan install .. --build=missing -s compiler.libcxx=libstdc++11
+        ;;
+    *)
+        conan install .. --build=missing -s compiler.libcxx=libstdc++
+        ;;
+esac
 popd
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DENABLE_SDL2_MIXER=OFF -DUSE_CONAN=ON
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DUSE_CONAN=ON
 cmake --build build --config Release
